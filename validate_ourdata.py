2f6969033562aff357c5b7393eb0745c56dfcecc
yueli <YueLi65@mail.ustc.edu.cn>
2023-06-19 14:05:19 +0800

    Update and rename validate_fk.py to validate_ourdata.py

--- validate_ourdata.py
+++ validate_ourdata.py
@@ -1,6 +1,4 @@
- # The train file for network
-# Based on pytorch 1.8
-# Use the docker: nlos_trans:1.8 in Ubuntu
+# Based on pytorch 1.10
 import os
 import sys
 import time
@@ -14,7 +12,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 import scipy.io as scio
-
+import cv2
 from util.SpadDataset import SpadDataset
 from util.SetRandomSeed import set_seed, worker_init
 from util.ParseArgs import parse_args
@@ -35,15 +33,14 @@ lsmx = torch.nn.LogSoftmax(dim=1)
 smx = torch.nn.Softmax(dim=1)
 from pro.Loss import criterion_KL, criterion_L2
 from skimage.metrics import structural_similarity as ssim
-from models import model_tst_NLOS_NBlks,model_tst_NLOS_NBlks_phy
+from models import nlost
 
 def main():
     
     # baseline   
-    # model = model_tst_NLOS_NBlks_phy.TSTIntemodelNBlk_LCGC_128_512_v2(ch_in=1)
-    model = model_tst_NLOS_NBlks_phy.TSTIntemodelNBlk_LCGC_128_512_v9_14(ch_in=1)
+    model = nlost.NLOST(ch_in=1, num_coders=1,spatial=128,tlen=256,bin_len=0.0096)
 
-    model_path = '/data/yueli/output/CVPR2023_nlosp/nlosp_v9_14_fk_LFEbike_Resize_color2gray_2022_1010/epoch_9_6000.pth'
+    model_path = ''
     model.cuda()
     model = torch.nn.DataParallel(model)
     # print(model)
@@ -59,12 +56,9 @@ def main():
     #    model_dict.update({k[7:]: ckpt_dict[k]})
     model.load_state_dict(model_dict)
     print("Start eval...")
-    # rw_path  = '/data/yueli/dataset/align_fk_256_512'
-    # rw_path  = '/data/yueli/dataset/xu_512'
-    rw_path  = '/data/yueli/dataset/xu_new_data128'
+    rw_path  = '/data/yueli/dataset/cvpr2023_data'
 
-    #rw_path = '/data1/pengjy/NLOS/Data_simu/real_world/align_fk_all'
-    out_path = '/data/yueli/output/nlost_v9_14_up_gray/niter6000/fk/'
+    out_path = ''
     if not os.path.exists(out_path):
         os.makedirs(out_path, exist_ok=True)
     all_file = []
@@ -75,20 +69,8 @@ def main():
     for i in range(len(all_file)): 
 
         transient_data = scio.loadmat(all_file[i])
-        # transient_data = transient_data['data'].transpose([2,1,0])  #sig final_meas measlr
-        # transient_data = transient_data['final_meas']  #sig final_meas measlr
-        # M_wnoise = np.asarray(transient_data).astype(np.float32).reshape([1,256,256,-1])   # 1, 1, 64, 64,2048  8ps
-        # transient_data = transient_data[:,::-1,:]
-        # M_wnoise = np.asarray(transient_data).astype(np.float32).reshape([1,512,512,-1])
-        # M_wnoise = M_wnoise[:,::2,:,:] + M_wnoise[:,1::2,:,:]
-        # M_wnoise = M_wnoise[:,:,::2,:] + M_wnoise[:,:,1::2,:] 
-        # M_wnoise = M_wnoise[:,::2,:,:] + M_wnoise[:,1::2,:,:]
-        # M_wnoise = M_wnoise[:,:,::2,:] + M_wnoise[:,:,1::2,:]
-
         transient_data = transient_data['data'].transpose([1,0,2])   #sig final_meas measlr
         M_wnoise = np.asarray(transient_data).astype(np.float32).reshape([1,128,128,-1])
-
-
         M_wnoise = np.ascontiguousarray(M_wnoise)
         M_wnoise = np.transpose(M_wnoise, (0, 3, 1, 2))  
         M_mea = torch.from_numpy(M_wnoise[None])  
@@ -99,7 +81,6 @@ def main():
             front_view = im_re.detach().cpu().numpy()[0, 0]
             front_dep = dep_re.detach().cpu().numpy()[0, 0]
             vlo = vlo_re.detach().cpu().numpy()[0, 0]
-            import cv2
             cv2.imwrite(out_path + f'/{i}.png', (front_view / np.max(front_view))*255)
             cv2.imwrite(out_path + f'/{i}d.png', (front_dep / np.max(front_dep))*255)
             # # import matplotlib.pyplot as plt
